7b9184b6257c4d3c3904e6926f9e822dad3ed69d
Knirag <k.niragire@alustudent.com>
2024-01-22 11:54:22 +0000

    ...

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" 3-main """
+"""
+3-main
+"""
 from base_caching import BaseCaching
 
 
@@ -18,19 +20,26 @@ class LIFOCache(BaseCaching):
             key: Key for the cache.
             item: Value associated with the key.
         """
-        if key or item is not None:
+        if key is not None and item is not None:
             value_cache = self.get(key)
             # Make a new entry if key doesn't exist
             if value_cache is None:
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                     keys_to_discard = list(self.cache_data.keys())
                     last_key_index = len(keys_to_discard) - 1
-                    del self.cache_data[keys_to_discard[last_key_index]]
-                    print("DISCARD: {}".format(keys_to_discard[last_key_index]))
-            # If the key already exists, delete it before updating
+                    del self.cache_data[keys_to_discard]
+                    print("DISCARD: {}".format(
+                        keys_to_discard[last_key_index]
+                        ))
             else:
                 del self.cache_data[key]
-            # Modify value
+
+            if key in self.least_recent:
+                self.least_recent.remove(key)
+                self.least_recent.append(key)
+            else:
+                self.least_recent.append(key)
+
             self.cache_data[key] = item
 
     def get(self, key):
